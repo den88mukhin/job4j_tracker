ebd8ef2f767b97defdf05b3e59cfee44cf0910a9
Denis Mukhin <mukhin88dm@yandex.ru>
2023-08-14 20:28:29 +0300

    6. Правила Checkstyle [#504981]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -1,23 +1,22 @@
 package ru.job4j.checkstyle;
 
 public class Broken {
+    private static final String NEW_VALUE = "";
     private int sizeOfEmpty = 10;
-    public String surname;
-    public static final String NEW_VALUE = "";
+    private String surname;
+    private String name;
 
-    void echo() { }
+    public Broken() { }
 
-    void media(Object obj) {
+    public void echo() { }
+
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) {
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
 
     }
-
-    String name;
-
-    Broken() { }
 }
